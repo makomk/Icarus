4fe51a1bdee9b3de486e30923a5c629c4f9c245c
Aidan Thornton <makosoft@googlemail.com>
2012-08-12 14:57:32 +0100

    New mkrelease script

--- FPGA_project/Src/mkrelease.sh
+++ FPGA_project/Src/mkrelease.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 set -e
-PREFIX="cm1out/shortfin_dcmwd3"
-VARIANTS="test_140 test_150 test_160 test_170_overclock test_180_overclock test_190_overclock test_200_overclock"
+mkdir -p cm1out
+PREFIX="cm1out/halffin"
+VARIANTS="test_140 test_150 test_160 test_170 test_180_overclock test_190_overclock test_200_overclock"
 
-egrep -v 'COMP "([RT]xD|extminer_[rt]xd<.>|dip<0>)" LOCATE' fpgaminer_top.pcf > "$PREFIX.pcf"
+#egrep -v 'COMP "([RT]xD|extminer_[rt]xd<.>|dip<0>)" LOCATE' fpgaminer_top.pcf > "$PREFIX.pcf"
+cp fpgaminer_top.pcf "$PREFIX.pcf"
 cp fpgaminer_top.ncd "$PREFIX.ncd"
-fpga_edline -e -p edit_pinout.scr "$PREFIX.ncd" "$PREFIX.pcf"
-par -ol high -xe n -k "$PREFIX.ncd" "$PREFIX"_par.ncd "$PREFIX.pcf"
-mv "$PREFIX"_par.ncd "$PREFIX.ncd"
+#fpga_edline -e -p edit_pinout.scr "$PREFIX.ncd" "$PREFIX.pcf"
+#par -ol high -xe n -k "$PREFIX.ncd" "$PREFIX"_par.ncd "$PREFIX.pcf"
+#mv "$PREFIX"_par.ncd "$PREFIX.ncd"
 
 for VARIANT in $VARIANTS; do
     cp "$PREFIX.ncd" "$PREFIX"_tmp.ncd
